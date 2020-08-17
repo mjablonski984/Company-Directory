c7283b831280934fa31f841fed1423836248ec5b
mjablonski984 <mjablonski984@gmail.com>
2020-08-17 17:26:06 +0200

    Initial commit

--- libs/php/config.php
+++ libs/php/config.php
@@ -1,14 +1,14 @@
 <?php
-	// $cd_host = getenv('HOST');
-	// $cd_user = getenv('USERNAME');
-	// $cd_password = getenv('PASSWORD');
-	// $cd_dbname = getenv('DB_NAME');
+	$cd_host = getenv('HOST');
+	$cd_user = getenv('USERNAME');
+	$cd_password = getenv('PASSWORD');
+	$cd_dbname = getenv('DB_NAME');
 	$cd_port = 3306;
 	$cd_socket = "";
 
-	// for localhost use :
-	$cd_host = "127.0.0.1";
-	$cd_user = "root";
-	$cd_password = "";
-	$cd_dbname = "companydirectory";
+	// // for localhost use :
+	// $cd_host = "127.0.0.1";
+	// $cd_user = "root";
+	// $cd_password = "";
+	// $cd_dbname = "companydirectory";
 ?>
